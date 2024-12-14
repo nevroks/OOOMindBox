79be8687e9fab89f0dce6f154ca35fc447c28f3b
Nikita Babitsyn <nitubro1617@gmail.com>
2024-12-14 14:13:58 +0500

    deploy changes

--- src/utils/router/router.tsx
+++ src/utils/router/router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import MainPage from '../../pages/MainPage/MainPage'
+import { APP_PATHS } from '../constants/appPaths'
 
 const router = createBrowserRouter([
-	{
-		index: true,
-		element: <MainPage />
-	}
+    {
+        path: `${import.meta.env.BASE_URL}/${APP_PATHS.MAIN_PAGE}`,
+        element: <MainPage />
+    }
 ])
 
 export default router
